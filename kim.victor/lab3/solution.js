5ca3cf4187dea981236ea4fa79b1edd329e0e21b
л1ьмщ <kimvo1511@gmail.com>
2026-09-10 18:48:36 +0300

    [TASK-3] variant-35 kim.victor

---
+++ kim.victor/lab3/solution.js
@@ -0,0 +1,14 @@
+export function findMedian(arr) {
+    if (arr.length === 0) {
+        return 0;
+    }
+
+    const sorted = [...arr].sort((a, b) => a - b);
+    const middle = Math.floor(sorted.length / 2);
+
+    if (sorted.length % 2 !== 0) {
+        return sorted[middle];
+    }
+
+    return (sorted[middle - 1] + sorted[middle]) / 2;
+}
